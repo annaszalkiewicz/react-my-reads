2cb1ecc9447237f2edfc922adbb4837cec330d32
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-07-20 23:13:34 +0200

    Add header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Tell me what are you reading or what would you like to read</h1>
+          <h2 className="App-subtitle">My Reads App</h2>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main className="container">
+        </main>
       </div>
     );
   }
 }
 
 export default App;
+
+/**
+ * 
+ */
