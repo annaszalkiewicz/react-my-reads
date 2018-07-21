7d6524e52022c6cc6aa75e1c2ded0d58daf168f0
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-07-21 17:21:38 +0200

    Edit header

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Tell me what are you reading or what would you like to read</h1>
-          <h2 className="App-subtitle">My Reads App</h2>
+          <div className="overlay"></div>
+          <div className="text-overlay">
+            <h1 className="App-title">Tell me what are you reading or what would you like to read</h1>
+            <h2 className="App-subtitle">My Reads App</h2>
+          </div>
         </header>
         <main className="container">
+          <section className="search" id="search">
+            <button className="submit">Search</button>
+          </section>
         </main>
         <footer className="footer">
           <p className="copyright">Written and coded by <a href="https://mywebgraphicdesign.com">Anna Szalkiewicz</a>
