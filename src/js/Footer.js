1fb83039f654df7bd90192f5322d2d2a6763a3cf
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-07-22 21:14:50 +0200

    Add rel attribute to link

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer(props) {
   return (
     <footer className="footer">
-    <p className="copyright">Written and coded by <a href="https://mywebgraphicdesign.com" target="_blank">
+    <p className="copyright">Written and coded by <a href="https://mywebgraphicdesign.com" rel="noopener noreferrer" target="_blank">
     Anna Szalkiewicz</a>
     </p>
   </footer>
