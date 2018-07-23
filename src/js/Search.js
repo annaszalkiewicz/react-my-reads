8c9acfd72d81b521b127112bffb39544e83b4ec6
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-07-23 23:21:17 +0200

    Add new SearchPage component

--- src/js/Search.js
+++ src/js/Search.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/App.css';
 
-
 class Search extends Component {
 	render () {
 		return (
--- src/js/SearchPage.js
+++ src/js/SearchPage.js
@@ -1,13 +1,79 @@
 import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import { search, getAll } from '../utils/BooksAPI';
 import '../css/App.css';
 
 class SearchPage extends Component {
-  render() {
-    return (
-      <form>
-        <input type="search" name="search-input" id="search"/>
-      </form>
 
+  state = {
+    books: [],
+    query: '',
+    foundBooks: [],
+  }
+  componentDidMount = (query) => {
+    getAll(query).then((books) => {
+      this.setState({ books });
+    });
+  }
+  updateQuery = (query) => {
+    this.setState({ query: query });
+
+    if (query) {
+      search(query).then((foundBooks) => {
+        if (foundBooks.length) {
+          this.setState({foundBooks})
+        }
+        else {
+          this.setState({foundBooks: []});
+        }
+      })
+    }
+    else {
+      this.setState({foundBooks: []});
+    }
+  }
+
+  clearQuery = () => {
+    this.setState({ query: '' })
+  }
+  render() {
+    let query = this.state.query;
+    let foundBooks = this.state.foundBooks;
+
+    return (
+
+      <main>
+        <form className="search-form">
+          <label htmlFor="search-input" className="input-label">Look for new book:</label>
+          <input
+            type="text"
+            id="search-input"
+            className="search-input"
+            placeholder="Seach by author or title"
+            value={query}
+            onChange={(event) => this.updateQuery(event.target.value, foundBooks)}
+          />
+        </form>
+
+        {foundBooks.length !== 0 && query.length !== 0 && (
+          <section id="found-books" className="found-books">
+            <ul className="books-list">
+              {foundBooks.map((book) => (
+                <li key={book.id}>
+                  {book.title}
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
+        {foundBooks.length === 0 && query.length !== 0 && (
+          <div className="no-found">
+            <p>Sorry, no book found :(</p>
+          </div>
+        )}
+
+      </main>
     );
   }
 
