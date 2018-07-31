636ae333498803633b02fd66c9c4c6aa38aa6dd9
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-07-31 21:51:25 +0200

    Rewrite Shelf to stateless functional component

--- src/js/Shelf.js
+++ src/js/Shelf.js
@@ -1,25 +1,22 @@
+// eslint-disable-next-line
 import React, { Component } from 'react';
 import '../css/App.css';
 import '../img/more-icon.svg';
 
+const Shelf = ({changeShelf, currentShelf, book}) => {
 
-class Shelf extends Component {
+	return (
+		<div className="shelf-changer">
+			<select value={currentShelf} onChange={(event) => changeShelf(book, event.target.value)}>
+				<option value="move" disabled>Move to...</option>
+				<option value="currentlyReading">Currently Reading</option>
+				<option value="wantToRead">Want to Read</option>
+				<option value="read">Read</option>
+				<option value="none">None</option>
+			</select>
+		</div>
+	);
 
-	render() {
-		const { changeShelf, currentShelf } = this.props;
-
-		return (
-			<div className="shelf-changer">
-				<select value={currentShelf} onChange={(event) => changeShelf(this.props.book, event.target.value)}>
-					<option value="move" disabled>Move to...</option>
-					<option value="currentlyReading">Currently Reading</option>
-					<option value="wantToRead">Want to Read</option>
-					<option value="read">Read</option>
-					<option value="none">None</option>
-				</select>
-			</div>
-		);
-	}
-}
+};
 
 export default Shelf;
