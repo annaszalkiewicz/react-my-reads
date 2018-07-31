36c8a697f6e6ac51f6e1458914ae26ee909534b5
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-07-31 14:23:09 +0200

    Rewrite Footer to stateless functional component

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-function Footer(props) {
-  return (
-    <footer className="footer">
-    <p className="copyright">Written and coded by <a href="https://mywebgraphicdesign.com" rel="noopener noreferrer" target="_blank">
-    Anna Szalkiewicz</a>
-    </p>
-  </footer>
-  )
-}
+const Footer = (() => {
+	return (
+		<footer className="footer">
+			<p className="copyright">Written and coded by <a href="https://mywebgraphicdesign.com" rel="noopener noreferrer" target="_blank">
+		Anna Szalkiewicz</a>
+			</p>
+		</footer>
+	);
+});
 
 export default Footer;
